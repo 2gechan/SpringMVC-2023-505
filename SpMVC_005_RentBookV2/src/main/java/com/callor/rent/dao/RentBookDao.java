7d0ba25a0686c265c96b1fa325cf31c772c8f43a
2gechan <7532602@gmail.com>
2023-07-26 15:21:28 +0900

    도서대여

---
+++ SpMVC_005_RentBookV2/src/main/java/com/callor/rent/dao/RentBookDao.java
@@ -0,0 +1,24 @@
+package com.callor.rent.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+
+import com.callor.rent.models.RentBookDto;
+
+public interface RentBookDao {
+	
+	@Select("select * from tbl_rent_book order by rent_seq")
+	public List<RentBookDto> selectAll();
+	
+	public RentBookDto findById(long id);
+	
+	public List<RentBookDto> findByBCode(String bcode);
+	public List<RentBookDto> findByMCode(String mcode);
+	
+	public int insert(RentBookDto dto);
+	public int update(RentBookDto dto);
+	
+	public void create_rentbook_table(String dumy);
+
+}
---
+++ SpMVC_005_RentBookV2/src/main/java/com/callor/rent/service/RentBookService.java
@@ -0,0 +1,13 @@
+package com.callor.rent.service;
+
+import java.util.List;
+
+import com.callor.rent.models.RentBookDto;
+
+public interface RentBookService {
+
+	public List<RentBookDto> selectAll();
+
+	public int insert(RentBookDto rentBookDto);
+
+}
